a876304715451c90529c109bed01c363e614ddba
kkkkkkkho <kunho3@naver.com>
2025-04-19 16:16:09 +0900

    feat: support ymd query param for full festival range

--- api/schedule.js
+++ api/schedule.js
@@ -1,11 +1,16 @@
 export default async function handler(req, res) {
+  const { ymd } = req.query;
+
+  if (!ymd || !/^\d{8}$/.test(ymd)) {
+    return res.status(400).send('Invalid or missing ymd');
+  }
+
   const response = await fetch("https://www.xticket.co.kr/MPTicketing/InfoWS/RIA.asmx/GetScheduleSummary", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
     },
-    body: "companyCd=SP0010&storeCd=01&planCompanyCd=PN0037&playYMD=20250503&scheCd=&riaType=N"
-
+    body: `companyCd=SP0010&storeCd=01&planCompanyCd=PN0037&playYMD=${ymd}&scheCd=&riaType=N`
   });
 
   const text = await response.text();
